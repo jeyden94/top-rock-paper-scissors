ac8ff3477af14de996569c582319ff79ce6f439b
jeyden94 <jeyden94@gmail.com>
2025-07-05 17:02:58 -0500

    Added human choice

--- java.js
+++ java.js
@@ -1,10 +1,18 @@
 function getComputerChoice() {
-    x = Math.floor(Math.random() * 100);
-    if (x <= 33) {
+    computerChoice = Math.floor(Math.random() * 100);
+    if (computerChoice <= 33) {
         console.log("Rock");
-    } else if (x > 33 && x<= 66) {
+    } else if (computerChoice > 33 && computerChoice<= 66) {
         console.log("Paper");
     } else {
         console.log("Scissors")
     }
+
+    return computerChoice;
+}
+
+function getHumanChoice() {
+    humanChoice = window.prompt("What will you play?", "Rock, Paper, Scissors");
+    
+    return humanChoice;
 }
