c723ac86b2e1943a847d5b43bd8d438b4718a87c
jeyden94 <jeyden94@gmail.com>
2025-07-05 17:44:42 -0500

    Some final cleanup to js file

--- java.js
+++ java.js
@@ -71,39 +71,3 @@ for (i = 0; i < 5; i++) {
     console.log(`Final Score: ${humanScore}-${computerScore}`);
 
 }
-
-
-
-
-
-// humanScore = 0;
-// computerScore = 0;
-
-// tempValue = Math.floor(Math.random() * 100);
-
-// if (tempValue <= 33) {
-//     console.log("Rock");
-//     computerChoice = "Rock";
-// } else if (tempValue > 33 && tempValue<= 66) {
-//     console.log("Paper");
-//     computerChoice = "Paper";
-// } else {
-//     console.log("Scissors")
-//     computerChoice = "Scissors";
-// }
-
-// humanChoice = window.prompt("What will you play?", "Rock, Paper, Scissors");
-
-
-// humanChoice = humanChoice.toLowerCase()[0].toUpperCase() + humanChoice.toLowerCase().slice(1);
-// computerChoice = computerChoice.toLowerCase()[0].toUpperCase() + computerChoice.toLowerCase().slice(1);
-
-// if ((humanChoice === "Paper" && computerChoice === "Rock") || (humanChoice === "Rock" && computerChoice === "Scissors")|| (humanChoice === "Scissors" && computerChoice === "Paper")) {
-//     console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
-//     humanScore++;
-// } else if (humanChoice === computerChoice) {
-//     console.log(`Both opponents chose ${humanChoice}, it's a tie!`);
-// } else {
-//     console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
-//     computerScore++;
-// } 
